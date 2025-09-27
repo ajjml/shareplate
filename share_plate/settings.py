508c06e095122fdf590f1efc4d649d8ab2334bfd
Muhammed Ajmal <muhammedajmal153153.com>
2025-09-27 13:27:18 +0530

    Remove SSL cert from DATABASES

--- share_plate/settings.py
+++ share_plate/settings.py
@@ -72,9 +72,11 @@ DATABASES = {
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT", "3306"),
+        'OPTIONS': {
+            'ssl': {'ca': BASE_DIR / 'certs/ca.pem'},
+        },
     }
 }
-
 # ========================
 # Password validation
 # ========================
