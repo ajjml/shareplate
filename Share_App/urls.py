a4b975714fbb4a5abe2823c56bdc7b08dd42e3c1
ajjml <muhammedajmal153153@gmail.com>
2025-10-16 14:54:39 +0530

    Update urls.py

--- Share_App/urls.py
+++ Share_App/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path
-from.import views
+from . import views
 from django.contrib.auth import views as auth_views
 
-urlpatterns=[
-    path('',views.home_page,name='home'),
+urlpatterns = [
+    path('', views.home_page, name='home'),
     path('about/', views.about_page, name='about'),
     path('contact/', views.contact_page, name='contact'),
-    path('login/',views.login_view,name='login'),
-    path('logout/',views.logout_view,name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('share_food/',views.share_food_view,name='share_food'),
-    path("delete_food/<int:pk>/", views.delete_food, name="delete_food"),
-    path("share_location/", views.share_location, name="share_location"),
-    path("delete_location/<int:pk>/", views.delete_location, name="delete_location"),
-        # Forgot password / Reset password
+    path('share_food/', views.share_food_view, name='share_food'),
+    path('delete_food/<int:pk>/', views.delete_food, name='delete_food'),
+    path('share_location/', views.share_location, name='share_location'),
+    path('delete_location/<int:pk>/', views.delete_location, name='delete_location'),
+
+    # Forgot password / Reset password
     path('password-reset/',
          auth_views.PasswordResetView.as_view(template_name='forgot_password.html'),
          name='password_reset'),
@@ -27,6 +28,4 @@ urlpatterns=[
     path('password-reset-complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
          name='password_reset_complete'),
-
-
-    ]
+]
